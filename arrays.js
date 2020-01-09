1ffe4a91050d4017f3f416bfc1b9e3b650d5d660
Nathan <7nater.nelson@gmail.com>
2020-01-09 01:48:53 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -39,7 +39,7 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  return array.pop();
+  return array.pop()
 }
 
 function removeElementFromEndOfArray(array) {
